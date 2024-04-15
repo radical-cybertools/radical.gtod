3e72ac2f201ca3bdd60186ef5f09a622cb520292
Andre Merzky <andre@merzky.net>
2024-04-15 08:09:03 +0200

    hotfix to get install working after setuptools update

--- setup.py
+++ setup.py
@@ -139,12 +139,9 @@ def get_version(_mod_root):
       # _sdist_name = _sdist_name.replace('#', '-')
       # _sdist_name = _sdist_name.replace('_', '-')
 
+        # setuptools 69.5 does changes naming scheme
         if not os.path.isfile('%s/%s' % (_path, _sdist_name)):
-            sdist_name = '%s-%s.tar.gz' % (name.replace('.', '_'), _version_base)
-
-        if not os.path.isfile('%s/%s' % (_path, _sdist_name)):
-            raise RuntimeError('Could not find sdist tarball %s/%s'
-                               % (_path, _sdist_name))
+            _sdist_name = '%s-%s.tar.gz' % (name.replace('.', '_'), _version_base)
 
         if '--record'    in sys.argv or \
            'bdist_egg'   in sys.argv or \
@@ -256,7 +253,7 @@ with open('%s/requirements.txt' % root, encoding='utf-8') as freq:
 #
 setup_args = {
     'name'               : name,
-    'namespace_packages' : ['radical'],
+  # 'namespace_packages' : ['radical'],
     'version'            : version,
     'description'        : 'returns seconds since epoch in subsecond resolution.',
     'author'             : 'RADICAL Team',
